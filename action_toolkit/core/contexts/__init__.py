6a89f377a069345d225a46c900544265f562073a
rhawk117 <ryan.hawkins@lexisnexisrisk.com>
2025-06-12 09:37:51 -0400

    setup: created "contexts" directory

---
+++ action_toolkit/core/contexts/__init__.py
@@ -0,0 +1,5 @@
+
+
+# Documentation for GitHub Action Contexts:
+# https://docs.github.com/en/actions/writing-workflows/choosing-what-your-workflow-does/accessing-contextual-information-about-workflow-runs#github-context
+
